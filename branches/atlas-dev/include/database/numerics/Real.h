8b6b0a428e6ca3c518148c2be9af0310daee57e8
sharker81 <sharker81@3418a986-edad-ce79-62c4-b09c02485035>
2014-05-05 03:20:14 +0000

    cleaning out development branch since it has been merged to trunk

--- branches/atlas-dev/include/database/numerics/Real.h
+++
@@ -1,8 +0,0 @@
-#ifndef CMDB_REAL_H
-#define CMDB_REAL_H
-
-#ifndef CHOMP_REAL_H
-typedef double Real;
-#endif
-
-#endif
--- branches/atlas-dev/script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#PBS -l nodes=15:ppn=8
-
-cd $PBS_O_WORKDIR
-mpiexec  ./Conley_Morse_Database $ENV_MODELDIR
