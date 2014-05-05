06960b3463b61d8c667369e8d5b03736d10f67d8
sharker81 <sharker81@3418a986-edad-ce79-62c4-b09c02485035>
2014-05-05 03:19:11 +0000

    Merging the development branch into trunk

---
+++ include/database/program/jobs/Compute_Morse_Graph.h
@@ -0,0 +1,36 @@
+/*
+ *  Compute_MorseGraph.h
+ */
+
+#ifndef _CMDP_COMPUTE_CONLEY_MORSE_GRAPH_
+#define _CMDP_COMPUTE_CONLEY_MORSE_GRAPH_
+
+#include <boost/shared_ptr.hpp>
+
+#include "database/structures/MorseGraph.h"
+#include "database/structures/Grid.h"
+
+/// Computes the Morse decomposition with respect to the given map
+/// on the given phase space, and creates its representation by means
+/// of a Conley-Morse graph.
+
+template < class Map >
+void Compute_Morse_Graph (MorseGraph * MG,
+                          boost::shared_ptr<Grid> phase_space,
+                          const Map & interval_map,
+                          const unsigned int Init,
+                          const unsigned int Min,
+                          const unsigned int Max,
+                          const unsigned int Limit);  
+
+template < class Map >
+void Compute_Morse_Graph (MorseGraph * MG,
+                          boost::shared_ptr<Grid> phase_space,
+                          const Map & interval_map,
+                          const unsigned int Min,
+                          const unsigned int Max,
+                          const unsigned int Limit);  
+
+#include "database/program/jobs/Compute_Morse_Graph.hpp"
+
+#endif
---
+++ source/program/Conley_Morse_Database.cpp
@@ -0,0 +1,94 @@
+/*
+ *  Conley_Morse_Database.cpp
+ */
+
+#include <ctime>
+#include <fstream>
+#define CMG_VERBOSE
+#include "delegator/delegator.h"  /* For Coordinator_Worker_Scheme<>() */
+#ifdef COMPUTE_MORSE_SETS
+#include "database/program/MorseProcess.h"
+#endif
+#ifdef COMPUTE_CONTINUATION
+#include "ModelMap.h"
+#include "database/structures/Database.h"
+#endif
+#ifdef COMPUTE_CONLEY_INDEX
+#include "database/program/ConleyProcess.h"
+#endif
+
+#ifdef USE_SDSL
+#include "database/structures/SuccinctGrid.h"
+#endif
+#include "database/structures/PointerGrid.h"
+#include "database/structures/UniformGrid.h"
+#include "database/structures/EdgeGrid.h"
+ 
+#include <boost/serialization/export.hpp>
+#ifdef USE_SDSL
+BOOST_CLASS_EXPORT_IMPLEMENT(SuccinctGrid);
+#endif
+BOOST_CLASS_EXPORT_IMPLEMENT(PointerGrid);
+BOOST_CLASS_EXPORT_IMPLEMENT(UniformGrid);
+BOOST_CLASS_EXPORT_IMPLEMENT(EdgeGrid);
+
+
+#include "database/structures/EuclideanParameterSpace.h"
+BOOST_CLASS_EXPORT_IMPLEMENT(EuclideanParameter);
+BOOST_CLASS_EXPORT_IMPLEMENT(EuclideanParameterSpace);
+
+int main ( int argc, char * argv [] ) {
+  delegator::Start ();
+#ifdef COMPUTE_MORSE_SETS
+  std::cout << "STARTING MORSE PROCESS\n";
+  time_t morsestarttime = time ( NULL );
+  delegator::Run < MorseProcess > (argc, argv);
+  time_t morsetime = time ( NULL ) - morsestarttime;
+  {
+  std::string filestring ( argv[1] );
+  std::string appendstring ( "/MorseProcessTime.txt" );
+  std::ofstream morsetimeoutfile ( (filestring + appendstring) . c_str () );
+  morsetimeoutfile << morsetime << " seconds.\n";
+  morsetimeoutfile . close ();
+  }
+#endif
+
+#ifdef COMPUTE_CONTINUATION
+  std::cout << "STARTING CONTINUATION PROCESS\n";
+  int comm_rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &comm_rank);
+  if ( comm_rank == 0 ) {
+    Database database;
+    {
+    std::string filestring ( argv[1] );
+    std::string appendstring ( "/database.raw" );
+    database . load ( (filestring + appendstring) . c_str () );
+    }
+    //database . removeBadBoxes<ModelMap> ();
+    database . postprocess ();
+    {
+    std::string filestring ( argv[1] );
+    std::string appendstring ( "/database.mdb" );
+    database . save ( (filestring + appendstring) . c_str () );
+    }
+  }
+#endif
+
+#ifdef COMPUTE_CONLEY_INDEX  
+  std::cout << "STARTING CONLEY PROCESS\n";
+  time_t conleystarttime = time ( NULL );
+  delegator::Run < ConleyProcess > (argc, argv);
+  time_t conleytime = time ( NULL ) - conleystarttime;
+  {
+  std::string filestring ( argv[1] );
+  std::string appendstring ( "/ConleyProcessTime.txt" );
+  std::ofstream conleytimeoutfile ( (filestring + appendstring) . c_str () );
+  conleytimeoutfile << conleytime << " seconds.\n";
+  conleytimeoutfile . close ();
+  }
+#endif
+  delegator::Stop ();
+
+
+  return 0;
+}
