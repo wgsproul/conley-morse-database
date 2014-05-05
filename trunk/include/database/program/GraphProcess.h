d3c05447ca0e490ea5f967cb83d46007e4ea2b3c
sharker81 <sharker81@3418a986-edad-ce79-62c4-b09c02485035>
2014-05-05 03:19:11 +0000

    Merging the development branch into trunk

---
+++ trunk/include/database/program/GraphProcess.h
@@ -0,0 +1,23 @@
+#ifndef CMDB_GRAPHPROCESS_H
+#define CMDB_GRAPHPROCESS_H
+
+#include "database/program/ComputeGraph.h"
+#include "ModelMap.h"
+
+#include "delegator/delegator.h"
+/* * * * * * * * * * * * * * */
+/* GraphProcess declaration */
+/* * * * * * * * * * * * * * */
+class GraphProcess : public Coordinator_Worker_Process {
+public:
+  void command_line ( int argc, char * argv [] );
+  void initialize ( void );
+  void work ( Message & result, const Message & job ) const;
+  void finalize ( void );
+private:
+  MapEvals evals;
+  chomp::Toplex toplex_;
+  ModelMap * f_;
+};
+
+#endif
